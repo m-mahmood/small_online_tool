d143476fabfc6c9e2190ab73b0d1deeac6ee7a36
Muhammad Mahmood <m.mahmood.web@gmail.com>
2026-01-15 14:01:18 +0500

    site verificatio updates

--- pages/apps.py
+++ pages/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
 class PagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'pages'
+    
+    # This helps Django locate the templatetags folder
+    def ready(self):
+        pass
--- pages/templatetags/seo_tags.py
+++ pages/templatetags/seo_tags.py
@@ -1,14 +1,14 @@
 from django import template
+from django.conf import settings  # <--- IMPORTANT: Import this
 
 register = template.Library()
 
 @register.inclusion_tag('seo_scripts.html', takes_context=True)
 def seo_scripts(context):
     request = context['request']
-    # Pass variables to the template
     return {
         'GOOGLE_ANALYTICS_ID': context.get('GOOGLE_ANALYTICS_ID'),
         'GOOGLE_SITE_VERIFICATION': context.get('GOOGLE_SITE_VERIFICATION'),
         'GOOGLE_ADSENSE_CLIENT_ID': context.get('GOOGLE_ADSENSE_CLIENT_ID'),
-        'DEBUG': context.get('DEBUG', True)
+        'debug': settings.DEBUG,  # Passes True/False to template
     }
