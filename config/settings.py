e3014fd8b5f0457b278c1f40a6bab51b02815eab
Muhammad Mahmood <m.mahmood.web@gmail.com>
2026-01-17 00:03:13 +0500

    contact issue fixed

--- config/settings.py
+++ config/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
         conn_max_age=600,
         conn_health_checks=True,
-        ssl_require=False
+        ssl_require=True
     )
 }
 
